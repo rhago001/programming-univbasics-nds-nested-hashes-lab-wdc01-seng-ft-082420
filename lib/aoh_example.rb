79632cea8e567d01a5ae689f03c3225306636c7c
Rahel Hagos <hagos.rahelh@gmail.com>
2020-08-08 22:02:53 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -26,7 +26,8 @@ end
 
 def aoh_update(aoh, row, key, new_value)
   
-  aoh[1][:name]=new_value
+  aoh[1][:name]= new_value
+  aoh_update
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
